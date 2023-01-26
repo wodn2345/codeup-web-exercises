5b0076a836b7b853595509a01040837776590e15
Jaewoo Baek <jaewoo.baek01@gmail.com>
2023-01-26 16:19:15 -0600

    loops exercises

---
+++ js/for_loops.js
@@ -0,0 +1,64 @@
+
+// function showMultiplicationTable(i)
+// var i=0;
+// while(i < 10) {
+//     console.log('while loop iteration #' * 7);
+//     i++;
+// }
+
+//Exercise 1
+
+// function showMultiplicationTable(number) {
+//     var i = 1;
+//     while (i < 11) {
+//         console.log( number + " * " + i + " = " + number * i );
+//         i++;
+//     }
+// }
+//Exercise 2
+//use for loop to get the random number between 20 and 200 and output to console whether
+//number is odd or even.
+
+// function randomNumber() {
+//     return Math.floor(Math.random() * 200);
+// }
+// function isOddorEven(a) {
+//     if (a % 2 === 0) {
+//         console.log(a + " is Even");
+//     } else {
+//         console.log(a + " is Odd")
+//     }
+// }
+//
+// function randomOddorEven () {
+//     for ( var i = 0; i < 10; i++) {
+//         isOddorEven(randomNumber())
+//     }
+// }
+// randomOddorEven();
+
+//Exercise 3
+// creat for loop that uses console.log to creat the out put shown below.
+
+
+// var i = 1;
+// function numberLine(a){
+//     return i * a
+// }
+//
+// for (; i < 10;) {
+//     console.log(i);
+//     i++;
+//     if(i===2) {
+//         console.log(i,i)
+//     } else if(i === 3) {
+//         console.log(i, i, i)
+//     } else if(i===4) {
+//         console.log(i,i,i,i)
+//     }
+// }
+
+//Exercise 5
+// for (let i = 0; i < 20; ++i) {
+//     console.log(100 -5 * i )
+// }
